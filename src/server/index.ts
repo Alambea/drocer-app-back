a952e86db1c1e4ebe0973747cf6497db6d895f0b
Ana Lambea <analambeap@gmail.com>
2023-09-04 10:11:47 +0200

    Set cors configuration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,19 @@
+import cors from "cors";
 import express from "express";
+
+const corsOptions = {
+  origin: [
+    "https://ana-lambea-final-project-20230.netlify.app/",
+    "http://localhost:4005",
+  ],
+  methods: "",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 const app = express();
 app.disable("x-powered-by");
+
+app.use(cors(corsOptions));
 
 export default app;
