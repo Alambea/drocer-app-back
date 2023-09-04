73be6dc3957b5b04f214873f1aa38a6fa06d0308
Ana Lambea <analambeap@gmail.com>
2023-09-04 12:43:12 +0200

    Add pingController

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import pingController from "./controllers/pingController.js";
 import { endPointNotFound, generalErrorHandler } from "./middleware/errors.js";
 
 const corsOptions = {
@@ -20,7 +21,10 @@ app.use(morgan("dev"));
 
 app.use(cors(corsOptions));
 
+app.get("/", pingController);
+
 app.use(endPointNotFound);
+
 app.use(generalErrorHandler);
 
 export default app;
