86192a6703d81672e22f69b9d478682ddf200590
Ana Lambea <analambeap@gmail.com>
2023-09-13 16:51:03 +0200

    Add deleteByIdController controller

--- src/server/router/recordsRouter.ts
+++ src/server/router/recordsRouter.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { paths } from "../paths/paths.js";
-import { getRecordsController } from "../controllers/recordsControllers.js";
+import {
+  deleteByIdController,
+  getRecordsController,
+} from "../controllers/recordsControllers.js";
 
 const recordsRouter = express.Router();
 
 recordsRouter.get(paths.slash, getRecordsController);
+recordsRouter.delete(`${paths.slash}:id`, deleteByIdController);
 
 export default recordsRouter;
